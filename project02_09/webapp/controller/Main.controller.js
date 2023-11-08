51b02868d0ec1492011b13dee75bfe4945d97339
geonjin <ca990125a@naver.com>
2023-11-08 09:27:35 +0900

    microchart 추가

--- project02_09/webapp/controller/Main.controller.js
+++ project02_09/webapp/controller/Main.controller.js
@@ -1,8 +1,6 @@
-sap
-    .ui
-    .define(
+sap.ui.define(
         [
-            "sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "sap/viz/ui5/data/FlattenedDataset", "sap/viz/ui5/controls/common/feeds/FeedItem"
+            "sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"
         ],
         /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -14,7 +12,7 @@ sap
                 onInit: function () {
                     //onInit 함수 탈때마다 밑에 함수로 이동
                     this._setChartInView();
-                    // this._setChartInController();
+                    
                 },
                 _setChartInView() {
 
@@ -25,8 +23,8 @@ sap
                     //var oModel = new JSONModel(oData); or var oModel = sap.ui.model.json.JSONModel(oData)
                     this.getView().setModel(oModel, 'main');
 
-                },
-                _setChartInController() {}
+                }
+                
             });
         }
     );
